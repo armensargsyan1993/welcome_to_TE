b45c349600f387d2aa8648b4408f05a19eb784d9
Armen Sargsyan <armensargsyan111991@gmail.com>
2022-11-22 00:38:38 +0400

    complete fourth task

--- task_4/index.jsx
+++ task_4/index.jsx
@@ -1,28 +1,35 @@
-import { Component, createRef } from 'react';
+import { Component, createRef } from "react";
 
 class MainComponent extends Component {
-    myRef = createRef(); // create simple ref
+  myRef = createRef(); // create simple ref
 
-    toggleChildVisibility = () => this.myRef.current.toggleButton(); // method to hide or show child component
+  toggleChildVisibility = () => this.myRef.current.toggleButton(); // method to hide or show child component
 
-    render() {
-        return (
-            <>
-                <button onClick={this.toggleChildVisibility}>toggle child component</button>
-                <ChildComponent ref={this.myRef} />  {/* set ref to controll child component */}
-            </>
-        );
-    }
-};
+  render() {
+    return (
+      <>
+        <button onClick={this.toggleChildVisibility}>
+          toggle child component
+        </button>
+        <ChildComponent ref={this.myRef} />{" "}
+        {/* set ref to controll child component */}
+      </>
+    );
+  }
+}
 
-class ChildComponent extends Component {
-    state = { isActive: true };
+const ChildComponent = forwardRef((_, ref) => {
+  const [isActive, setIsActive] = useState(true);
 
-    toggleButton = () => this.setState({ isActive: !this.state.isActive });
+  const toggleButton = () => {
+    setIsActive((prev) => !prev);
+  };
 
-    render() {
-        return (
-            this.state.isActive ? <div>child component</div> : null
-        );
-    }
-};
+  useEffect(() => {
+    ref.current = {
+      toggleButton,
+    };
+  }, []);
+
+  return isActive ? <div>child component</div> : null;
+});
